6f1281b3e63024711dded2e6f0344fddaa7d5bfc
riiiido <121432050+just-npc@users.noreply.github.com>
2025-05-06 22:56:19 +0700

    add main.js with local storage set up functions

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 // Do your work here...
-console.log('Hello, world!');
+console.log("Hello, world!");
+
+let books = [
+  /* {
+    id: number | string,
+    title: string,
+    author: string,
+    year: number,
+    isCompleted: boolean
+  } */
+];
+
+const SAVED_EVENT = "saved-book";
+const STORAGE_KEY = "BOOKSHELF_APPS";
+
+// fungsi untuk memeriksa apakah browser support local storage
+function isStorageExist() {
+  if (typeof Storage === undefined) {
+    alert("browser kamu tidak mendukung local storage");
+    return false;
+  }
+
+  return true;
+}
+
+// fungsi untuk menyimpan ke local storage
+function saveData() {
+  if (isStorageExist()) {
+    const parsed = JSON.stringify(books);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
+  }
+}
